6f195e18166c41ec94b5e6eb28b97f31337d9008
SmileY <smileyznl@gmail.com>
2025-01-30 13:53:59 -0300

    Fix code

--- spread/Spread.cpp
+++ spread/Spread.cpp
@@ -4,11 +4,11 @@ CSpread gSpread;
 
 void CSpread::ServerActivate()
 {
-    static cvar_t Spread_Divisor = { "sc_divisor", strdup("2.0"), FCVAR_SERVER | FCVAR_PROTECTED, 2.0f, NULL };
+    static cvar_t Spread_Divisor = {"sc_divisor", strdup("2.0"), FCVAR_SERVER | FCVAR_PROTECTED, 2.0f, NULL};
 
-	g_engfuncs.pfnCVarRegister(&Spread_Divisor);
+    g_engfuncs.pfnCVarRegister(&Spread_Divisor);
 
-	this->m_Spread_Divisor = g_engfuncs.pfnCVarGetPointer(Spread_Divisor.name);
+    this->m_Spread_Divisor = g_engfuncs.pfnCVarGetPointer(Spread_Divisor.name);
 }
 
 float CSpread::GetSpread(CBasePlayer *Player, float vecSpread)
