64d354e50a63ec7cf170de85a19702affeb57562
Victor <victoradcarvalho@hotmail.com>
2025-01-30 19:14:37 -0300

    post-build script

--- spread/PostBuild.ps1
+++ spread/PostBuild.ps1
@@ -1,5 +1,5 @@
 # HLDS Path
-$hldsPath = "D:\Files\Desktop\hlds";
+$hldsPath = "D:\steamcmd\steamapps\common\Half-Life";
 
 # Test HLDS Path
 if (!(Test-Path($hldsPath)))
@@ -9,18 +9,18 @@ if (!(Test-Path($hldsPath)))
 }
 
 # Executable path
-$hldsExecutable = "D:\Files\Desktop\hlds\hlds.exe";
+$hldsExecutable = "D:\steamcmd\steamapps\common\Half-Life\hlds.exe";
 
 # Plugin Name
 $pluginName = ((Get-Item .).Name).ToLower();
 
 # Release DLL
-$releaseDLL = "$PSScriptRoot\Release\$($pluginName)_mm.dll"
+$releaseDLL = "$PSScriptRoot\build\bin\Release\$($pluginName)_mm.dll"
 
 # Test if was build
 if (!(Test-Path($releaseDLL)))
 {
-    Write-Host("Failed do run HLDS: $($releaseDLL) was not found or not build.") -ForegroundColor yellow;
+    Write-Host("Failed do run HLDS: $($releaseDLL) was not found or not built.") -ForegroundColor yellow;
     Exit 0
 }
 
@@ -31,10 +31,10 @@ $hldsIP = (Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias Ethernet).IPAddr
 $hldsPort = 27020
 
 # Executable parameters
-$hldsParam = "-console -game cstrike -port $hldsPort -pingboost 3 -steam -master -secure -bots -timeout 3 +ip $hldsIP +map de_inferno +maxplayers 32 +sys_ticrate 1000";
+$hldsParam = "-console -game cstrike -port $hldsPort -pingboost 3 -steam -master -secure -bots -timeout 3 +ip $hldsIP +map de_nuke +maxplayers 32 +sys_ticrate 1000";
 
 # DLL Path
-$dllPath = "$hldsPath\cstrike\addons\$pluginName\dlls";
+$dllPath = "$hldsPath\cstrike\addons\$pluginName";
 
 # Resolve path of released dll
 $releaseDLL = (Resolve-Path($releaseDLL)).Path;
@@ -45,40 +45,17 @@ $hlds = Get-Process -Name hlds -ErrorAction SilentlyContinue;
 # If found
 if ($hlds)
 {
-    # If is started 
-    if (!$hlds.started)
-    {
-        # Get HLDS path
-        $hldsPath = (Get-Item $hlds.Path).DirectoryName;
-
-        # Set HLDS executable path
-        $hldsExecutable = $hlds.Path;
-        
-        # Set DLL path
-        $dllPath = "$hldsPath\cstrike\addons\$pluginName\dlls";
-
-        # Force kill
-        $hlds | Stop-Process -Force -ErrorAction SilentlyContinue
-    }
+    $hlds | Stop-Process -Force -ErrorAction SilentlyContinue
 }
 
 # Wait 1 second
 Start-Sleep(1);
 
-# Check if path exists
-if(Test-Path($releaseDLL))
-{
-    # Copy DLL tao path
-    Copy-Item  -Path $releaseDLL -Destination $dllPath -Recurse -Force
+# Copy DLL to path
+Copy-Item  -Path $releaseDLL -Destination $dllPath -Recurse -Force
 
-    # Start HLDS
-    Start-Process -FilePath $hldsExecutable -ArgumentList $hldsParam -WorkingDirectory $hldsPath -WindowStyle Minimized
+# Start HLDS
+Start-Process -FilePath $hldsExecutable -ArgumentList $hldsParam -WorkingDirectory $hldsPath -WindowStyle Minimized
 
-    # Success
-    Write-Host("Running HLDS: IP is $($hldsIP)") -ForegroundColor green;
-}
-else
-{
-    # Failed to run
-    Write-Host("Failed to run HLDS: Path not found $($dllPath)") -ForegroundColor red;
-}
+# Success
+Write-Host("Running HLDS: IP is $($hldsIP)") -ForegroundColor green;
