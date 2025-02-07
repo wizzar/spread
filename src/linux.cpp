1a47fa515b4374d3849f4511bacff295ef56dfc7
Victor <victoradcarvalho@hotmail.com>
2025-02-07 19:46:17 -0300

    linux defs

---
+++ src/linux.cpp
@@ -0,0 +1,13 @@
+
+#ifndef _WIN32
+#define _GLIBCXX_USE_CXX11_ABI 0
+#define _stricmp strcasecmp
+#define _strnicmp strncasecmp
+#define _strdup strdup
+#define _unlink unlink
+#define _vsnprintf vsnprintf
+#define _write write
+#define _close close
+#define _acces access
+#define _vsnwprintf vswprintf
+#endif
