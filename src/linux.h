fac83057d8419798f255e39f1830a2db7db8b7ad
Victor <victoradcarvalho@hotmail.com>
2025-02-08 11:18:33 -0300

    make

--- src/linux.h
+++ src/linux.h
@@ -1,14 +1,14 @@
-
-#ifndef _WIN32
-#define _GLIBCXX_USE_CXX11_ABI 0
-#define _stricmp strcasecmp
-#define _strnicmp strncasecmp
-#define _strdup strdup
-#define _unlink unlink
-#define _vsnprintf vsnprintf
-#define _write write
-#define _close close
-#define _acces access
-#define _vsnwprintf vswprintf
-#define _snprintf snprintf
-#endif
+//
+//#ifndef _WIN32
+//#define _GLIBCXX_USE_CXX11_ABI 0
+//#define _stricmp strcasecmp
+//#define _strnicmp strncasecmp
+//#define _strdup strdup
+//#define _unlink unlink
+//#define _vsnprintf vsnprintf
+//#define _write write
+//#define _close close
+//#define _acces access
+//#define _vsnwprintf vswprintf
+//#define _snprintf snprintf
+//#endif
